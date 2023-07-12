14fc81ff11054370012256f8b7631f7c0c2036ee
Hausel Kinga <hausel.kinga@gmail.com>
2023-07-12 11:45:52 +0200

    consume API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'WeatherApp';
+export class AppComponent implements OnInit {
+  constructor(private weatherService: WeatherService) {}
+
+  weatherData?: WeatherData
+
+  ngOnInit(): void {
+    this.weatherService.getWeatherData('london')
+    .subscribe({
+      next: (response) => {
+        this.weatherData = response;
+      }
+    })
+  }
 }
