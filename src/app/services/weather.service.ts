57de3314cc6fec6db00f80c5ceffb3e32bfa8c23
Hausel Kinga <hausel.kinga@gmail.com>
2023-07-12 11:20:08 +0200

    create interfaces for API response

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../environment/environment';
+import { environment } from '../environments/environment';
+import { Observable } from 'rxjs/internal/Observable';
+import { WeatherData } from '../models/weather.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +11,11 @@ export class WeatherService {
 
   constructor(private http: HttpClient) { }
 
-  getWeatherData(cityName: string) {
-    this.http.get(environment.weatherApiBaseUrl, {
+  getWeatherData(cityName: string): Observable<WeatherData> {
+    return this.http.get<WeatherData>(`${environment.weatherApiBaseUrl}/${cityName}`, {
       headers: new HttpHeaders()
         .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
-        .set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKeyHeaderValue),
-      params: new HttpParams()
-        .set("q", cityName)
-        .set("units", "metric")
-        .set("mode", "json")
+        .set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKeyHeaderValue)
     })
   }
 }
